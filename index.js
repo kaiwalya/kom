ff902b8b385844e677d3be0de3a7d4dfc79cba2e
Kaiwalya Kher <knkher@gmail.com>
2014-06-29 04:12:25 -0700

    Hello world streaming

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  var n = 10;
+  var sleepTime = 1000;
+  var replySome = function () {
+  	n--;
+  	var msg = 'Hello World ' + n + "\n"
+  	if (n > 0) {
+  		res.write(msg);
+  		setTimeout(replySome, sleepTime);
+  	}
+  	else {
+  		res.end(msg);
+  	}
+  };
+
+  setTimeout(replySome, sleepTime);
+
 }).listen(8000, '0.0.0.0');
 console.log('Server running at http://0.0.0.0:8000/');
