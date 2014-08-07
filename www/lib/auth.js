4f20605d11679abd0a02624d30fef0f5333e24e3
Kaiwalya Kher <knkher@gmail.com>
2014-08-06 23:32:11 -0700

    Handle that fbInit and document load might happen in any order.

--- www/lib/auth.js
+++ www/lib/auth.js
@@ -1,5 +1,69 @@
 (function () {
 
+	var LoginManager = function () {
+		var This = this;
+		This._status = null;
+		This._fbResponseCallback = This._processResponse.bind(This);
+		This._initFBDone = false;
+		This._initLoginElementDone = false;
+	};
+
+	LoginManager.prototype._processResponse = function (response) {
+		var This = this;
+		
+		if (response.status === 'connected') {
+			This._loginElement.text = This._loginElement.getAttribute("logoutText") || "Logout";
+		}
+		else {
+			This._loginElement.text = This._loginElement.getAttribute("loginText") || "Login"; 
+		}
+		This._status = response.status;
+		This._authResponse = response.authResponse;
+	};
+
+	LoginManager.prototype._init = function () {
+		var This = this;
+		if (!This._initFBDone) {
+			return;
+		}
+		if (!This._initLoginElementDone) {
+			return;
+		}
+		window.FB.getLoginStatus(This._fbResponseCallback);
+	};
+
+	LoginManager.prototype._onLoginClicked = function () {
+		var This = this;
+		if (This._status === 'connected') {
+			window.FB.logout(This._fbResponseCallback);
+		}
+		else {
+			window.FB.login(This._fbResponseCallback);
+		}
+	};
+
+	LoginManager.prototype.doElementInit = function (element) {
+		var This = this;
+		This._loginElement = element;
+		This._loginElement.addEventListener('click', This._onLoginClicked.bind(This));
+		This._initLoginElementDone = true;
+		This._init();
+	};
+
+	LoginManager.prototype.doFBInit = function (params) {
+		var This = this;
+		window.FB.init(params);
+		This._initFBDone = true;
+		This._init();
+	};
+
+	var loginManager = new LoginManager();
+
+	window.addEventListener('load', function load () {
+		window.removeEventListener('load', load);
+		loginManager.doElementInit(window.document.getElementById("loginButton"));
+	});
+
 	var appId = '524759647625736';
 	var hostname = window.location.hostname;
 	if (
@@ -11,7 +75,7 @@
 		appId = '524756560959378';
 	}
 	window.fbAsyncInit = function() {
-		window.FB.init({
+		loginManager.doFBInit({
 			appId: appId,
 			xfbml: false,
 			version: 'v2.0'
@@ -29,48 +93,5 @@
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 
-	var LoginManager = function (loginElement) {
-		var This = this;
-		This._loginElement = loginElement;
-		This._loginElement.addEventListener('click', This._onLoginClicked.bind(This));
-
-		This._status = null;
-		This._fbResponseCallback = This._processResponse.bind(This);
-		This._init();
-	};
-
-	LoginManager.prototype._processResponse = function (response) {
-		var This = this;
-		console.log(response);
-		
-		if (response.status === 'connected') {
-			This._loginElement.text = This._loginElement.getAttribute("logoutText") || "Logout";
-		}
-		else {
-			This._loginElement.text = This._loginElement.getAttribute("loginText") || "Login"; 
-		}
-		This._status = response.status;
-		This._authResponse = response.authResponse;
-	};
-
-	LoginManager.prototype._init = function () {
-		var This = this;
-		window.FB.getLoginStatus(This._fbResponseCallback);
-	};
-
-	LoginManager.prototype._onLoginClicked = function () {
-		var This = this;
-		if (This._status === 'connected') {
-			window.FB.logout(This._fbResponseCallback);
-		}
-		else {
-			window.FB.login(This._fbResponseCallback);
-		}
-	};
-
-	window.addEventListener('load', function load () {
-		window.removeEventListener('load', load);
-		new LoginManager(window.document.getElementById("loginButton"));
-	});
 })();
 
